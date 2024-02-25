b8cf11f290f260e9a63f187fb28de4cf84d55d0f
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2024-02-25 13:48:01 -1000

    chore: quick utility package

---
+++ lib/firestore_serialization.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library firestore_serialization;
+
+export 'src/firestore_serialization.dart';
---
+++ lib/src/firestore_serialization.dart
@@ -0,0 +1,7 @@
+/// {@template firestore_serialization}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class FirestoreSerialization {
+  /// {@macro firestore_serialization}
+  const FirestoreSerialization();
+}
